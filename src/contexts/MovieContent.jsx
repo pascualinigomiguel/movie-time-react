ae8c6a2a30c345a500b5cca75a39752f2a13d8aa
unknown <pascualinigomiguel@gmail.com>
2026-09-23 23:14:14 +0800

    initial commit

---
+++ src/contexts/MovieContent.jsx
@@ -0,0 +1,42 @@
+import {createContext, useState, useContext, useEffect} from "react"
+
+const MovieContent = createContext()
+
+export const useMovieContent = () => useContext(MovieContent)
+
+export const MovieProvider = ({children}) => {
+    const [favorites, setFavorites] = useState([])
+
+    useEffect(() => {
+        const storedFavs = localStorage.getItem("favorites")
+
+        if (storedFavs) setFavorites(JSON.parse(storedFavs))
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem('favorites', JSON.stringify(favorites))
+    }, [favorites])
+
+    const addToFavorites = (movie) => {
+        setFavorites(prev => [...prev, movie])
+    }
+
+    const removeFromFavorites = (movieId) => {
+        setFavorites(prev => prev.filter(movie => movie.id !== movieId))
+    }
+    
+    const isFavorite = (movieId) => {
+        return favorites.some(movie => movie.id === movieId)
+    }
+
+    const value = {
+        favorites,
+        addToFavorites,
+        removeFromFavorites,
+        isFavorite
+    }
+
+    return <MovieContent.Provider value={value}>
+        {children}
+    </MovieContent.Provider>
+}
